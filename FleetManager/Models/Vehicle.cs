0776eb62925b2d0640dab127556fd45d851fe519
Joseph <blazej.sabisz@cyfroweszkoly.pl>
2026-03-12 09:33:49 +0100

    Model danych Vehicle

--- FleetManager/Models/Vehicle.cs
+++ FleetManager/Models/Vehicle.cs
@@ -1,6 +1,28 @@
+using System.Reflection.Metadata.Ecma335;
+using ReactiveUI.Fody.Helpers;
+
 namespace FleetManager.Models;
 
 public class Vehicle
 {
-    
+   [Reactive]
+   public string  Name { get; set; } = string.Empty;
+   
+   
+   [Reactive]
+   public string  RegistrationNumber {
+      get;
+      set;
+   } = string.Empty;
+   [Reactive] public int FuelLevel {
+      get;
+      set;
+   } = 0;
+
+   [Reactive]
+   public string Status
+   {
+      get;
+      set;
+   } = string.Empty;
 }
