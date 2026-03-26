4bf9a83de8d85236563ea795ba4399e15a6dfa20
Filip <filip.flicinski@cyfroweszkoly.pl>
2026-03-26 23:53:40 +0100

    create detail window for vehicle status manipulating

---
+++ FleetManager/Views/VehicleDetailWindow.axaml.cs
@@ -0,0 +1,37 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using FleetManager.Models;
+using System;
+
+namespace FleetManager.Views;
+
+public partial class VehicleDetailWindow : Window
+{
+    public VehicleDetailWindow()
+    {
+        InitializeComponent();
+    }
+
+    public VehicleDetailWindow(Vehicle vehicle)
+    {
+        InitializeComponent();
+
+        DataContext = vehicle;
+
+        var comboBox = this.FindControl<ComboBox>("StatusComboBox");
+        if (comboBox != null)
+        {
+            comboBox.ItemsSource = Enum.GetValues(typeof(VehicleStatus));
+        }
+    }
+
+    private void CloseButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        this.Close();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
--- FleetManager/Views/VehicleItemView.axaml.cs
+++ FleetManager/Views/VehicleItemView.axaml.cs
@@ -1,5 +1,8 @@
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
+using FleetManager.Models;
+using FleetManager.Views;
 
 namespace FleetManager.Views;
 
@@ -8,6 +11,17 @@ public partial class VehicleItemView : UserControl
     public VehicleItemView()
     {
         InitializeComponent();
+
+        this.PointerPressed += VehicleItemView_PointerPressed;
+    }
+
+    private void VehicleItemView_PointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (DataContext is Vehicle vehicle)
+        {
+            var window = new VehicleDetailWindow(vehicle);
+            window.Show();
+        }
     }
 
     private void InitializeComponent()
